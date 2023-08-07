d52e9c39c2424ad95c8a412c8681626bab5b76cc
weisonx <578822454@qq.com>
2023-08-07 16:27:24 +0800

    Update 重复数字.cpp

--- Project/剑指offer/重复数字.cpp
+++ Project/剑指offer/重复数字.cpp
@@ -5,10 +5,19 @@
 using namespace std;
 
 //size:n, data[i]:0~n-1
-int duplicate_digital(vecotr<int> &data)
+int duplicate_digital(vector<int> &data)
 {
     for(int i = 0; i < data.size(); i++)
     {
-        
+        while(data[i] != i)
+        {
+            if(data[data[i]] == data[i])
+            {
+                return data[i];
+            }
+            swap(data[i], data[data[i]]);
+        }
     }
+
+    return -1;
 }
