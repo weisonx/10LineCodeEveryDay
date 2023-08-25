a7aa052ed7a609a43f8798203a6cd840a0f35e4a
weisonx <578822454@qq.com>
2023-08-25 15:55:50 +0800

    Create 打印从1到最大的n位数.cpp

---
+++ Project/剑指offer/打印从1到最大的n位数.cpp
@@ -0,0 +1,34 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+void print_one_to_max(int n)
+{
+    string number;
+
+    number.resize(n);
+    
+    helper(number, 0);
+}
+
+void helper(string &number, int index)
+{
+    if(index >= number.size())
+    {
+        return;
+    }
+
+    //最后一位赋值才打印；全0和前导0需要格式化。
+    if(index == number.size() - 1)
+    {
+        cout << number <<endl;
+        return;
+    }
+
+    for(int i = 0; i != 10; i++)
+    {
+        number[index] = i + '0';
+        helper(number, index+1);
+    } 
+}
